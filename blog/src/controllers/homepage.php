51150ae396030b5926eb1c06ff426d6bd73c9ec6
Lewis Kidd <lkidd@gk-optical.com>
2022-10-11 16:35:30 +0200

    added controller and model for homepage and posts

---
+++ blog/src/controllers/homepage.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('src/lib/database.php');
+require_once('src/model/post.php');
+
+use Application\Model\Post\PostRepository;
+
+function callHomepage()
+{
+	$postRepository = new PostRepository();
+	$postRepository->connection = new DatabaseConnection();
+	$posts = $postRepository->getPosts();
+
+	require('templates/homepage.php');
+}
